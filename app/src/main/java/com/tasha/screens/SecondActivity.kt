fee1e416a44ad8cb5ca0f2bde9c2a19758719004
Puru Chauhan <puru@nurture.farm>
2023-10-05 10:55:38 +0530

    added else for when block in SecondActivity

--- app/src/main/java/com/tasha/screens/SecondActivity.kt
+++ app/src/main/java/com/tasha/screens/SecondActivity.kt
@@ -51,6 +51,9 @@ class SecondActivity: AppCompatActivity() {
                     binding.textView.text = name
                 }
             }
+            else -> {
+
+            }
         }
     }
 }
