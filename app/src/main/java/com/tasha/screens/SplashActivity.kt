f34ac23b3f698a82f061e8c4963b43730fa00269
Puru Chauhan <puru@nurture.farm>
2024-02-12 14:55:56 +0530

    added fullscreen logic

--- app/src/main/java/com/tasha/screens/SplashActivity.kt
+++ app/src/main/java/com/tasha/screens/SplashActivity.kt
@@ -1,23 +1,39 @@
 package com.tasha.screens
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-
-import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import android.view.View
+import android.view.WindowInsets
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.tasha.databinding.ActivitySplashBinding
 
 
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivitySplashBinding
+    private lateinit var binding: ActivitySplashBinding
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        show()
 
         binding.buttonTrigger.setOnClickListener {
             when {
@@ -27,15 +43,48 @@ class SplashActivity : AppCompatActivity() {
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     startWork()
                 }
+
                 else -> {
 
                     requestPermissionLauncher.launch(
-                        android.Manifest.permission.POST_NOTIFICATIONS)
+                        android.Manifest.permission.POST_NOTIFICATIONS
+                    )
                 }
             }
         }
-
     }
+
+
+    private fun show() {
+
+        // Delayed removal of status and navigation bar
+        if (Build.VERSION.SDK_INT >= 30) {
+            binding.parentView.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            window
+                .decorView
+                .systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController = ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
 
     fun startWork() {
         startActivity(Intent(this, MainActivity::class.java))
@@ -43,10 +92,10 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private val requestPermissionLauncher = registerForActivityResult(
-        RequestPermission()
+        ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
         if (isGranted) {
-           startWork()
+            startWork()
         } else {
             // Explain to the user that the feature is unavailable because the
             // feature requires a permission that the user has denied. At the
